5b8fdbe52fcdded8b6590f1d53c315b710d1c514
PangMo5 <a3457709@icloud.com>
2021-05-14 17:24:17 +0900

    Conflict Test

---
+++ TuistSampleProject/ClassA.swift
@@ -0,0 +1,8 @@
+//
+//  ClassA.swift
+//  TuistSampleProject
+//
+//  Created by PangMo5 on 2021/05/14.
+//
+
+import Foundation
---
+++ TuistSampleProject/ClassC.swift
@@ -0,0 +1,8 @@
+//
+//  ClassC.swift
+//  TuistSampleProject
+//
+//  Created by PangMo5 on 2021/05/14.
+//
+
+import Foundation
