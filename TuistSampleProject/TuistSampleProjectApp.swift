2fc5fa2635fac14d09911f9b95ed43d6004ec75e
PangMo5 <a3457709@icloud.com>
2021-05-14 17:18:43 +0900

    Initial Commit

---
+++ TuistSampleProject/TuistSampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuistSampleProjectApp.swift
+//  TuistSampleProject
+//
+//  Created by PangMo5 on 2021/05/14.
+//
+
+import SwiftUI
+
+@main
+struct TuistSampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
